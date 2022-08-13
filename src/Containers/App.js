670b7ba13deb4e27bc618b51fb1d61cfe993e27f
arbiva <arbiva@post.bgu.ac.il>
2022-08-14 01:43:24 +0300

    first commit

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,59 +1,56 @@
-import React, { Component } from 'react';
-import CardList from '../Components/CardList';
-import SearchBox from '../Components/SearchBox';
-import Scroll from '../Components/Scroll';
-import ErrorBoundry from '../Components/ErrorBoundry';
+import React, { useState, useEffect } from "react";
+import CardList from "../Components/CardList";
+import SearchBox from "../Components/SearchBox";
+import Scroll from "../Components/Scroll";
+import ErrorBoundry from "../Components/ErrorBoundry";
 import "../index.css";
 import "./App.css";
 
-//state can change our app, and it is usually lives in the 
-//parent component, that can pass the state to the different components. 
+//state can change our app, and it is usually lives in the
+//parent component, that can pass the state to the different components.
 //so we could access "robots" by: "this.state.robots"
-//now our state - that is "robots" is passed down as props, 
+//now our state - that is "robots" is passed down as props,
 //so CardList accepts "robots" as props, eventhough in the App.js
-//they are state. 
+//they are state.
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      robots:[],
-      searchfield: ""
-    }
-  }
+function App() {
+  // when App renders, it first creats the states:
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
+  // then fetches the list of robots:
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
-      .then(users => this.setState({robots:users}));
-  }
-
+      //and then the users will update the robots state using the new users
+      .then(users => {setRobots( users )});
+    //the empty [] is a dependency array- a shortcut for "componentDidMount"
+    //when there is something inside, App will re-render everytime that
+    //something changes
+  }, []);
 
   //building methods of our own, require us to use the syntax of an arrow function
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value})
-  }
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
+  };
 
-  render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
-    if (!this.state.robots.length){
-      return <h1>Loading...</h1>
-    } else {
-        return (
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+
+  return !robots.length ? (
+    <h1>Loading...</h1>
+  ) : (
     <div className="App">
-      <h1 className='appHeadline'>ROBOFRIENDS</h1>
-      <SearchBox searchChange={this.onSearchChange}/>
+      <h1 className="appHeadline">ROBOFRIENDS</h1>
+      <SearchBox searchChange={onSearchChange} />
       <Scroll>
         <ErrorBoundry>
-         <CardList robots={filteredRobots}/>
+          <CardList robots={filteredRobots} />
         </ErrorBoundry>
       </Scroll>
     </div>
   );
-    }
-  }
 }
 
 export default App;
